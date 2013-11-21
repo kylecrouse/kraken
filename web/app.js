de3737a249cea55ac2a4291b2056c0d4715cfaed
Kyle Crouse <kyle.crouse@gmail.com>
2013-11-21 12:03:07 -0500

    getting smart and making q-mongo work correctly!

--- web/app.js
+++ web/app.js
@@ -1,24 +1,37 @@
 'use strict';
 
 var path = require('path');
+var url = require('url');
 
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 9000;
 
 var QMongoDB = require('q-mongodb');
+var mongoConnectionStr = process.env.MONGOHQ_URL || 'mongodb://127.0.0.1:27017/kraken';
 
-app.use(express.static(path.resolve(__dirname + '/../app')));
+app.configure(function () {
+  app.use(express.static(path.resolve(__dirname + '/../app')));
+});
 
 app.get('/api/', function (req, res, next) {
-  res.send('111');
-  // QMongoDB.db('kraken').then(function (db) {
-  //   return QMongoDB.collection(db, 'venue');
-  // }).then(function (collection) {
-  //   // Perform standard collection operations here
-  // }).then(function () {
-  //   return QMongoDB.closeAll();
-  // });
+  QMongoDB.connect(mongoConnectionStr)
+    .invoke('collection', 'test')
+    .invoke('remove')
+    .invoke('insert', { a: 1, b: 2 })
+    .then(function () {
+      return QMongoDB.connect(mongoConnectionStr)
+        .invoke('collection', 'test')
+        .invoke('findOne', { a: 1 })
+        .then(function (items) {
+          res.send(items);
+        });
+    })
+    .fail(function (err) {
+      console.log(err);
+      res.send('' + err);
+    })
+    .done();
 });
 
 app.listen(port);
